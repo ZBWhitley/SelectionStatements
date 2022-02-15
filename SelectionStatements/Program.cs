901045e966ec540fb068907ec2e90750beee7634
Zachary Whitley <zbwhitley@gmail.com>
2022-02-15 10:16:14 -0600

    first commit

--- SelectionStatements/Program.cs
+++ SelectionStatements/Program.cs
@@ -6,27 +6,52 @@ namespace SelectionStatements
     {
         static void Main(string[] args)
         {
-            var r = new Random();
-            var favNumber = r.Next(1, 1000);
+            //var r = new Random();
+            //var favNumber = r.Next(1, 1000);
 
-            Console.WriteLine("Please guess my favorite number:");
-            var userInput = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Please guess my favorite number:");
+            //var userInput = int.Parse(Console.ReadLine());
 
-            if (userInput > 27)
-            {
-                Console.WriteLine("Too high");
-            }
+            //if (userInput > 27)
+            //{
+            //    Console.WriteLine("Too high");
+            //}
 
-            else if (userInput < 27)
-            {
-                Console.WriteLine("Too Low");
-            }
+            //else if (userInput < 27)
+            //{
+            //    Console.WriteLine("Too Low");
+            //}
 
-            else 
-            {
+            //else 
+            
                 Console.WriteLine("Congratulations, you won!");
-            }
 
-        }
+
+                Console.WriteLine("What is your favorite school subject?");
+                var favoriteSubject = Console.ReadLine();
+
+
+                switch (favoriteSubject.ToLower())
+                {
+                    case "math":
+                        Console.WriteLine("Your favorite subject is math!");
+                        break;
+                    case "english":
+                        Console.WriteLine("Your favorite subject is english!");
+                        break;
+                    case "history":
+                        Console.WriteLine("Your favorite subject is history!");
+                        break;
+                    case "science":
+                        Console.WriteLine("Your favorite subject is science!");
+                        break;
+                    default:
+                        Console.WriteLine("Please select from math, english, history, or science.");
+                        break;
+
+
+                }
+
+            }
     }
 }
