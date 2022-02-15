843cc1ffb7f15218e7c967a902e2a4665895b360
Zachary Whitley <zbwhitley@gmail.com>
2022-02-15 09:19:04 -0600

    Add project files.

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    internal class random
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            Console.WriteLine("Please guess my favorite number:");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput > 27)
+            {
+                Console.WriteLine("Too high");
+            }
+
+            else if (userInput < 27)
+            {
+                Console.WriteLine("Too Low");
+            }
+
+            else 
+            {
+                Console.WriteLine("Congratulations, you won!");
+            }
+
+        }
+    }
+}
